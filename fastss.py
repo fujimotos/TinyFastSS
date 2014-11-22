4da291b8972e1f600883b2c54f012080db927c71
Fujimoto <fujimoto@writingarchives.sakura.ne.jp>
2014-11-22 23:51:44 +0900

    Implement the disk-based index dictionary.

--- fastss.py
+++ fastss.py
@@ -1,35 +1,54 @@
 """ A simple implementation of FastSS """
 
+import dbm
+import pickle
+
+KEY_ENCODING = 'utf8'
 
 class FastSS:
-    def __init__(self, indexdb=None):
-        if indexdb is None:
-            indexdb = {}
+    def __init__(self, indexdb):
         self.indexdb = indexdb
+
+    @classmethod
+    def open(cls, dbpath, flag='c'):
+        return cls(dbm.open(dbpath, flag))
+
+    def close(self):
+        self.indexdb.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
 
     @staticmethod
     def indexkeys(word):
-        res = {word}
-        wlen = len(word)
+        res, wlen = {word}, len(word)
+
         for i in range(wlen):
             for j in range(i, wlen):
                 key = word[:i] + word[i+1:j] + word[j+1:]
                 res.add(key)
-        return res
+
+        return {s.encode(KEY_ENCODING) for s in res}
 
     def add(self, word):
         for key in self.indexkeys(word):
-            if key not in self.indexdb:
-                self.indexdb[key] = set()
-            self.indexdb[key].add(word)
+            value = {word}
+
+            if key in self.indexdb:
+                value |= self.indexdb[key]
+
+            self.indexdb[key] = pickle.dumps(value)
 
     def get(self, word):
-        result = [[], [], []]
+        result = ([], [], [])
         candidate = set()
 
         for key in self.indexkeys(word):
             if key in self.indexdb:
-                candidate.update(self.indexdb[key])
+                candidate.update(pickle.loads(self.indexdb[key]))
 
         for cand in candidate:
             dist = editdist(word, cand)
