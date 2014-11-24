006b63d3fbcc559ed7c01f770006a6605793bd03
Fujimoto <fujimoto@writingarchives.sakura.ne.jp>
2014-11-25 00:15:02 +0900

    Improve the test cases

--- test/test_fastss.py
+++ test/test_fastss.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import tempfile
+import itertools
 import shutil
 from fastss import FastSS
 
@@ -33,14 +34,29 @@ class TestWriteGzip(unittest.TestCase):
 
     def test_create_database(self):
         dbpath = os.path.join(self.tmpdir, self.DBNAME)
+        max_dist = 2
 
-        with FastSS.open(dbpath, 'n', max_dist=2) as fastss:
-            fastss.add('test')
+        with FastSS.open(dbpath, 'n', max_dist=max_dist) as fastss:
+            pass
 
         with FastSS.open(dbpath, 'r') as fastss:
-            self.assertEqual(fastss.get('test'), {0: ['test'], 1: [], 2: []})
-            self.assertEqual(fastss.get('tes'),  {0: [], 1: ['test'], 2: []})
-            self.assertEqual(fastss.get('taste'),  {0: [], 1: [], 2: ['test']})
+            self.assertEqual(fastss.max_dist, max_dist)
+
+    def test_add_words(self):
+        dbpath = os.path.join(self.tmpdir, self.DBNAME)
+        word_set = ('0', '1', '00', '01', '10', '11', '000', '001', '010', '011',
+                    '100', '101', '110', '111', '0000', '0001', '0010', '0011',
+                    '0100', '0101', '0110', '0111', '1000', '1001', '1010', '1011',
+                    '1100', '1101', '1110', '1111')
+
+        with FastSS.open(dbpath, 'n') as fastss:
+            for word in word_set:
+                fastss.add(word)
+
+            res = fastss.get('1')
+            self.assertEqual(set(res[0]), {'1'})
+            self.assertEqual(set(res[1]), {'0', '01', '10', '11'})
+            self.assertEqual(set(res[2]), {'111', '001', '010', '100', '011', '101', '110', '00'})
 
 if __name__ == '__main__':
     unittest.main()
