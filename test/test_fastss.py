c34846dd81692ab60aba40fb212e0d7b161be4d0
Fujimoto <fujimoto@writingarchives.sakura.ne.jp>
2014-11-24 12:46:01 +0900

    Improve the test cases to fit the new semantics.

--- test/test_fastss.py
+++ test/test_fastss.py
@@ -14,20 +14,23 @@ class TestWriteGzip(unittest.TestCase):
         shutil.rmtree(self.tmpdir)
 
     def test_indexkeys(self):
-        keys = FastSS.indexkeys('test')
+        keys = FastSS.indexkeys('test', 1)
+        self.assertEqual(keys, {b'test', b'est', b'tst', b'tet', b'tes'})
+
+        keys = FastSS.indexkeys('test', 2)
         self.assertEqual(keys, {b'test', b'est', b'tst', b'tet', b'tes',
                                 b'st', b'et', b'es', b'tt', b'ts', b'te'})
 
     def test_create_database(self):
         dbpath = os.path.join(self.tmpdir, self.DBNAME)
 
-        with FastSS.open(dbpath, 'n') as fastss:
+        with FastSS.open(dbpath, 'n', dist=2) as fastss:
             fastss.add('test')
 
         with FastSS.open(dbpath, 'r') as fastss:
-            self.assertEqual(fastss.get('test'), (['test'], [], []))
-            self.assertEqual(fastss.get('tes'), ([], ['test'], []))
-            self.assertEqual(fastss.get('taste'), ([], [], ['test']))
+            self.assertEqual(fastss.get('test'), {0: ['test'], 1: [], 2: []})
+            self.assertEqual(fastss.get('tes'),  {0: [], 1: ['test'], 2: []})
+            self.assertEqual(fastss.get('taste'),  {0: [], 1: [], 2: ['test']})
 
 if __name__ == '__main__':
     unittest.main()
