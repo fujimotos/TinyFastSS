54e9d656ff1fccb4e109d26541c9d572e35855ae
Fujimoto <fujimoto@writingarchives.sakura.ne.jp>
2014-12-06 17:46:52 +0900

    Add tests for remove() method

--- test/test_fastss.py
+++ test/test_fastss.py
@@ -40,6 +40,37 @@ class TestManipulateDB(unittest.TestCase):
             self.assertEqual(set(res[1]), {'0', '01', '10', '11'})
             self.assertEqual(set(res[2]), {'111', '001', '010', '100', '011', '101', '110', '00'})
 
+    def test_remove_words(self):
+        dbpath = os.path.join(self.tmpdir, self.DBNAME)
+        word_set = ('0', '1', '00', '01', '10', '11', '000', '001', '010', '011',
+                    '100', '101', '110', '111', '0000', '0001', '0010', '0011',
+                    '0100', '0101', '0110', '0111', '1000', '1001', '1010', '1011',
+                    '1100', '1101', '1110', '1111')
+
+        with FastSS.open(dbpath, 'n') as fastss:
+            for word in word_set:
+                fastss.add(word)
+
+            fastss.remove('1')
+            fastss.remove('01')
+            fastss.remove('001')
+
+            res = fastss.get('1')
+            self.assertEqual(set(res[0]), set())
+            self.assertEqual(set(res[1]), {'0', '10', '11'})
+            self.assertEqual(set(res[2]), {'111', '010', '100', '011', '101', '110', '00'})
+
+    def test_remove_nonexist(self):
+        dbpath = os.path.join(self.tmpdir, self.DBNAME)
+
+        with FastSS.open(dbpath, 'n') as fastss:
+            fastss.add('potato')
+
+            with self.assertRaises(KeyError):
+                fastss.remove('tomato')
+
+            with self.assertRaises(KeyError):
+                fastss.remove('otato')
 
 class TestCreateIndex(unittest.TestCase):
     def test_indexkeys(self):
