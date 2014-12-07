e65e35761f9267f419afaf7914b1c8d381a00cf5
Fujimoto <fujimoto@writingarchives.sakura.ne.jp>
2014-12-07 21:19:01 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='TinyFastSS',
+    version='20141207',
+    py_modules=['fastss'],
+    author='Fujimoto Seiji',
+    author_email='fujimoto@writingarchives.sakura.ne.jp',
+    url='https://github.com/fujimotos/TinyFastSS',
+    description='An efficient indexing data structure for string similarity search',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing :: Indexing'
+    ]
+)
