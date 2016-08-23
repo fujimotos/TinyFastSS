cdf09a9df38c71c457ab2d1730189aeca6e5d973
Fujimoto Seiji <fujimoto@ceptord.net>
2016-08-24 00:53:19 +0900

    Add license and long_description arguments to setup().
    It is recommended to specify license and long_description in the
    setup() function. For example, if we omit the 'license' field,
    PKG-INFO states that the library license is UNKNOWN.

--- setup.py
+++ setup.py
@@ -8,6 +8,13 @@ setup(
     author_email='fujimoto@ceptord.net',
     url='https://github.com/fujimotos/TinyFastSS',
     description='An efficient indexing data structure for string similarity search',
+    long_description="""FastSS is an efficient indexing data structure
+for string similarity search, invented by researchers at Zurich University
+in 2007.
+
+TinyFastSS is a simple Python implementation of FastSS, written in less than
+300 LoC.""",
+    license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
